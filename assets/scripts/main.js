0a744e5fbe4c336cdc491cdae028f6292350d643
Marcelo Shen <caogangshen@gmail.com>
2023-05-21 23:36:31 -0700

    solve folder conflict

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -53,7 +53,8 @@ function initializeServiceWorker() {
     // B3. TODO - Register './sw.js' as a service worker (The MDN article
     //            "Using Service Workers" will help you here)
     try {
-      const registration = await navigator.serviceWorker.register("/sw.js", {
+      const swLocation = window.origin + "/sw.js";
+      const registration = await navigator.serviceWorker.register(swLocation, {
         scope: "/",
       });
       // B4. TODO - Once the service worker has been successfully registered, console
